840e6aa9b19884bc3f14425035412fc5bc5bb617
Tarun Saxena <121081303+tarunsaxena2000@users.noreply.github.com>
2024-05-08 18:02:43 +0530

    a web appliaction

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
+import { PageNotFoundComponentComponent } from './components/page-not-found-component/page-not-found-component.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {component:AboutComponent,path:'about'},
+  {component:HomeComponent,path:'home'},
+  {path:'',redirectTo:"home",pathMatch:'full'},
+  {component:LoginComponent,path:'login'},
+  {component:SignupComponent,path:'signup'},
+  {component:PageNotFoundComponentComponent,path:'**'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { PageNotFoundComponentComponent } from './components/page-not-found-component/page-not-found-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    LoginComponent,
+    SignupComponent,
+    PageNotFoundComponentComponent
   ],
   imports: [
     BrowserModule,
